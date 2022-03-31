df683dcd19f34bfe3b6d354d753ea9d6e9e52c6d
FrancoRomero11 <romerofran151@gmail.com>
2022-03-31 17:05:32 -0300

    cambios constantes

---
+++ Bootstrap-5/Variable-Constante/index.js
@@ -0,0 +1,47 @@
+//funcion clasica
+function mostrar(){
+    //declaramos una variable
+    let numero = 15
+    //se i,prime en la consola del navegador
+    console.log(numero)
+}
+
+function evaluar(){
+
+    let dato = 22;
+//condicion 
+    if(dato>10) {
+        let dato = 77
+        console.log(dato)
+    }
+    console.log(dato)
+}
+
+function validar(){
+
+   const valor = prompt('ingrese calificacion');
+
+   //forma 1
+   /* if(valor >= 7){
+       alert ('Estas aprobado!')
+   }else{
+       alert('Nos vemos en diciembre!')
+   }*/
+
+    //expresion ternaria
+   (valor >= 7) ? alert('Estas aprobado') : alert('Desaprobado')
+}
+//invocamos la funcion validar 
+//validar() 
+
+function calcular(){
+    //forma 1
+    const dato = document.getElementById("inp_dato1").value
+
+    //forma2 
+    //const dato = document.querySelector("#inp_dato1").value
+
+    const resultado = dato * 10;
+
+    alert (resultado)
+}
---
+++ Ejercicio 1/partedeejerccio.js
@@ -0,0 +1,7 @@
+function calcular(){
+    const dato = document.getElementById("inp_dato1").value
+
+    const dato2 = document.getElementById("imp_dato1").value
+
+    const resultado = dato * dato2
+}
